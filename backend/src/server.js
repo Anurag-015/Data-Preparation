87a39f2ab80dc9ab1ffa81036423b58eb6244bdc
Anurag-015 <mr.anuraggupta2003@gmail.com>
2025-11-14 00:24:38 +0530

    Update3rd

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ import { connectDb } from "./db.js";
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "https://data-preparation-mu.vercel.app/email" }));
+app.use(cors({ origin: "https://data-preparation-mu.vercel.app/" }));
 
 let collections;
 
